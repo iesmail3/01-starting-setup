56ee9aa930d83c4e74e57a4a12e6a21bbcb0d1bd
imran esmail <iesmail@d-tree.org>
2022-09-13 09:19:40 +0300

    Expanded the code to include additional fields

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
-function ExpenseItem(){ 
-    return <h2>Expense Item</h2>
+function ExpenseItem() {
+  return (
+    <div>
+      <div>March 28,2021</div>
+      <div>
+        <h2>Expense Item</h2>
+        <div>$245.95</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
