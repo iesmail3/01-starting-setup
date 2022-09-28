6c5f9cea291797501aefafc3bd0d664f35597a9b
imran esmail <iesmail@d-tree.org>
2022-09-28 15:35:11 +0300

    Decoupled expense date from expense item component

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -8,7 +8,7 @@ function ExpenseDate(props){
     <div className='expense-date'>
       <div className='expense-date__month'>{month}</div>
       <div className='expense-date__year'>{year}</div>
-      <div className='expense-date__date'>{date}</div>
+      <div className='expense-date__date'>{date}</div> 
     </div>
     ); 
 }
